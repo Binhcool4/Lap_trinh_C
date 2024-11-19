96980cd5a5340428263ece86677fd3347bcbd956
Binhcool4 <binhtrieuhn99@gmail.com>
2024-11-19 23:45:24 +0700

    Add files via upload

---
+++ Session 2/Btvn 2 Session2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	// Nhap bien chua gia tri so nguyen
+	int num1 = 15;
+	int num2 = 20;
+	
+	//Nhap bien chua gia tri tong
+	int Total;
+	Total = num1 + num2;
+	
+	//Nhap xuat du lieu
+	printf("Tong cua hai gia tri la %d", Total);
+	
+	return 0;
+}
