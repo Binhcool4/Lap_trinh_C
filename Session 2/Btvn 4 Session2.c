51abcb2647b65ad0ced20c3cc14fcdee8b95b265
Binhcool4 <binhtrieuhn99@gmail.com>
2024-11-20 10:21:30 +0700

    Add files via upload

--- Session 2/Btvn 4 Session2.c
+++ Session 2/Btvn 4 Session2.c
@@ -8,6 +8,11 @@ int main(){
 	Perimeter = squareEdge * 4;
 	printf ("Chu vi hinh vuong la %d ", Perimeter);
 	
+	//Tinh dien tich hinh vuong
+	int Acreage;
+	Acreage = squareEdge * squareEdge;
+	printf ("\nDien tich hinh vuong la %d", Acreage);
+	
 	 
 	return 0;
 }
