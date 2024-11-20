493417798caa61a1f4470ba66507effc7e66c2a6
Binhcool4 <binhtrieuhn99@gmail.com>
2024-11-20 09:56:28 +0700

    Add files via upload

---
+++ Session 2/Btvn 3 Session2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	int num1 = 15;
+	int num2 = 20;
+	
+	//Bien hieu
+	int Minus;
+	Minus = num1 - num2;
+	printf ("Hieu cua hai bien la %d", Minus);
+	
+	//Bien tich
+	int multi;
+	multi = num1 * num2;
+	printf ("\n Tich cua hai bien la %d", multi);
+	
+	//Bien thuong
+	int division;
+	division = num1 / num2;
+	printf ("\n Thuong cua hai bien la %d", division);
+	
+	return 0;
+}
