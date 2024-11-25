93c63c75c4c2a5962e98aa599662969be970a397
Binhcool4 <binhtrieuhn99@gmail.com>
2024-11-26 00:04:37 +0700

    Add files via upload

--- Session 5/Btvn 2 Session5.c
+++ Session 5/Btvn 2 Session5.c
@@ -4,14 +4,19 @@ int main(){
 	int b;
 	printf("Moi ban nhap so cua b : ");
 	scanf("%d", &b);
-	for(i = b; i>0; i--){
-		if(i == 44){
-			break;
-			
-		}
-		printf("%d \n", i);
-	}
-
 	
-	return 0;
+    if (b < 44) {
+        printf("Gia tri nhap vao phai lon hon hoac bang 44.\n");
+        return 1; 
+    }
+
+    for (i = b; i > 0; i--) {
+        if (i == 44) {
+            break;
+        }
+        printf("%d \n", i);
+    }
+
+    return 0;
 }
+
--- Session 5/Btvn 3 Session5.c
+++ Session 5/Btvn 3 Session5.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
-int main(){
-	int i = 1;
-	int b;
-	printf("Nhap so nguyen duong b : ");
-	scanf("%d", &b);
-	
-	//Vong lap
-	do{
-		int total;
-	    total = i + b;
-		printf("Tong cua hai so la : %d \n", total);
-		i++;
-		
-	}while(i<b);
-	
-	return 0;
+int main() {
+    int i = 1;
+    int b;
+    int sum = 0; 
+
+    printf("Nhap so nguyen duong b : ");
+    scanf("%d", &b);
+    
+    if (b <= 0) {
+        printf("Vui long nhap so nguyen duong.\n");
+        return 1;  
+    }
+
+    //Vong lap
+    do {
+        sum = sum + i;  
+        i++;        
+    } while (i <= b);  
+    
+   
+    printf("Tong cua cac so tu 1 den %d la: %d\n", b, sum);
+
+    return 0;
 }
+
