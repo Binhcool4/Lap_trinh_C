4ec358b8553c2f57cad64dfce01d56067b81db63
Binhcool4 <binhtrieuhn99@gmail.com>
2024-11-25 09:45:31 +0700

    Add files via upload

--- Session 4/Btvn 9 Session4.c
+++ Session 4/Btvn 9 Session4.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int isValidDate(int day, int month, int year) {
+int validDate(int day, int month, int year) {
     if (month < 1 || month > 12) return 0;  
 
     int maxDay;
 
-    // Xac dinh thang 
     if (month == 2) {
         // Kiem tra thang hai
         maxDay = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
@@ -33,11 +32,14 @@ int main() {
     printf("Nhap nam: ");
     scanf("%d", &year);
 
-    if (isValidDate(day, month, year)) 
+    if (validDate(day, month, year)){ 
         printf("Ngay %d/%d/%d hop le.\n", day, month, year);
-    else 
-        printf("Ngay %d/%d/%d khong hop le.\n", day, month, year);
+        
+    }else{
+    	printf("Ngay %d/%d/%d khong hop le.\n", day, month, year);
 
+	} 
+        
     return 0;
 }
 
